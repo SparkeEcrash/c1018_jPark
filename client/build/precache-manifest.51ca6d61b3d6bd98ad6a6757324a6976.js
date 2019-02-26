b756430cb83b9267c5d6b3e587fa5bf0ca85d54e
SparkeEcrash <jpark121989@gmail.com>
2019-02-26 12:44:45 -0800

    add bundled file

---
+++ client/build/precache-manifest.51ca6d61b3d6bd98ad6a6757324a6976.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac08c1934259686ee02a",
+    "url": "/static/js/main.ac08c193.chunk.js"
+  },
+  {
+    "revision": "25662c269fca324ea296",
+    "url": "/static/js/1.25662c26.chunk.js"
+  },
+  {
+    "revision": "ac08c1934259686ee02a",
+    "url": "/static/css/main.9522529f.chunk.css"
+  },
+  {
+    "revision": "5e24aef0c4b106842c48622e844bfec3",
+    "url": "/index.html"
+  }
+];
